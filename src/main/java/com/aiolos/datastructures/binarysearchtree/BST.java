faab8ad8b2dccb817d12013f94f5bc7e9f1936dd
aioloscn <756243011@qq.com>
2018-11-19 16:03:42 +0800

    'BST'

--- src/main/java/com/aiolos/datastructures/binarysearchtree/BST.java
+++ src/main/java/com/aiolos/datastructures/binarysearchtree/BST.java
@@ -37,37 +37,56 @@ public class BST<E extends Comparable<E>> {
      * @param e
      */
     public void add(E e) {
-
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
+        root = add(root, e);
     }
 
     /**
      * 向以node为根的二分搜索树中插入元素e，递归算法
+     * 返回插入新节点后二分搜索数的根
      * @param node
      * @param e
      */
-    private void add(Node node, E e) {
+    private Node add(Node node, E e) {
 
-        if (e.equals(node.e))
-            return;
-        else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+        if (node == null) {
             size ++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size ++;
-            return;
+            return new Node(e);
         }
 
         if (e.compareTo(node.e) < 0)
-            add(node.left, e);
+            node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+        return node;
+    }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (node == null)
+            return false;
+        if (e.compareTo(node.e) == 0)
+            return true;
+        else if (e.compareTo(node.e) < 0)
+            return contains(node.left, e);
         else
-            add(node.right, e);
+            return contains(node.right, e);
+    }
+
+    /**
+     * 二分搜索数的前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 }
