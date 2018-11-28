a01d2ce39f03ba1498c776fca2b70b4d00e7d07c
aioloscn <756243011@qq.com>
2018-11-29 00:13:08 +0800

    'segmenttree'

--- src/main/java/com/aiolos/datastructures/segmenttree/Test.java
+++ src/main/java/com/aiolos/datastructures/segmenttree/Test.java
@@ -6,8 +6,16 @@ package com.aiolos.datastructures.segmenttree;
 public class Test {
     public static void main(String[] args) {
 
-        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+               /* -2
+                1   -3
+             -2  3 -5  2
+           -2  0 */
+        Integer[] nums = {-2, 0, 3, -5, 2};
         SegmentTree<Integer> segTree = new SegmentTree<Integer>(nums, (a, b) -> a + b);
+        System.out.println(segTree);
+
+//        System.out.println(segTree.query(2, 4));
+        segTree.set(1, 6);
         System.out.println(segTree);
     }
 }
