818e697b3b8f2729382ae1bb506412ad786fba21
aioloscn <756243011@qq.com>
2018-12-10 23:42:28 +0800

    'avltree'

--- src/main/java/com/aiolos/datastructures/avltree/AVLTree.java
+++ src/main/java/com/aiolos/datastructures/avltree/AVLTree.java
@@ -1,5 +1,7 @@
 package com.aiolos.datastructures.avltree;
 
+import java.util.ArrayList;
+
 /**
  * Created by aiolos on 2018-12-05.
  */
@@ -36,6 +38,47 @@ public class AVLTree<K extends Comparable<K>, V> {
         return size == 0;
     }
 
+    /**
+     * 判断该二叉树是否是一棵二分搜索树
+     * @return
+     */
+    public boolean isBST() {
+
+        ArrayList<K> keys = new ArrayList<K>();
+        inOrder(root, keys);
+        for (int i = 1; i < keys.size(); i ++)
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0)
+                return false;
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys) {
+
+        if (node == null)
+            return;
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    /**
+     * 判断以Node为根的二叉树是否是一棵平衡二叉树
+     * @return
+     */
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+
+        if (node == null)
+            return true;
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
     private int getHeight(Node node) {
 
         if (node == null)
@@ -53,6 +96,42 @@ public class AVLTree<K extends Comparable<K>, V> {
         if (node == null)
             return 0;
         return getHeight(node.left) - getHeight(node.right);
+    }
+
+    /**
+     * 对节点y进行向右旋转操作，返回旋转后新的根节点x
+     * @param y
+     * @return
+     */
+    private Node rightRotate(Node y) {
+
+        Node x = y.left;
+        Node T3 = x.right;
+        // 向右旋转
+        x.right = y;
+        y.left = T3;
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
+    }
+
+    /**
+     * 对节点y进行向左旋转操作，返回旋转后新的根节点x
+     * @param y
+     * @return
+     */
+    private Node leftRatate(Node y) {
+
+        Node x = y.right;
+        Node T2 = x.left;
+        // 向左旋转
+        x.left = y;
+        y.right = T2;
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
     }
 
     /**
@@ -89,9 +168,24 @@ public class AVLTree<K extends Comparable<K>, V> {
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
         // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
-        if (Math.abs(balanceFactor) > 1) {
-
+        // 平衡维护
+        // LL
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+        // RR
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRatate(node);
+        // LR
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRatate(node.left);
+            return rightRotate(node);
         }
+        // RL
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRatate(node);
+        }
+
         return node;
     }
 
