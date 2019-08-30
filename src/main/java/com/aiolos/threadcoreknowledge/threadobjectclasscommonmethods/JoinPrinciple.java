159e864c52459171e40a9802627a756f0f604050
aiolos <15988346908@163.com>
2019-08-30 23:59:08 +0800

    join principle

---
+++ src/main/java/com/aiolos/threadcoreknowledge/threadobjectclasscommonmethods/JoinPrinciple.java
@@ -0,0 +1,32 @@
+package com.aiolos.threadcoreknowledge.threadobjectclasscommonmethods;
+
+/**
+ * join的代替写法，分析join原理
+ * @author Aiolos
+ * @date 2019-08-30 20:47
+ */
+public class JoinPrinciple {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "执行完毕");
+            }
+        });
+
+        thread.start();
+        System.out.println("开始等待子线程运行完毕");
+//        thread.join();
+        synchronized (thread) {
+            thread.wait();
+        }
+        System.out.println("所有子线程执行完毕");
+    }
+}
